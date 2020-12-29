81e2194b3335e74b387747d34d508fb545fb43fa
rohitparab <rohit.parab@ves.ac.in>
2020-12-29 17:40:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,10 +31,7 @@ memeForm.addEventListener("submit", function(event){
     bottomTextDiv.classList.add("text", "bottom");
     bottomTextDiv.innerText = document.getElementById("text-bottom").value;
 
-    var removeDiv = document.createElement('div');
-    removeDiv.classList.add("red-cross");
-    removeDiv.innerText = "X";
-    removeDiv.style.color = "red";
+   
     
 
 
@@ -42,7 +39,7 @@ listGallery.appendChild(memeLi);
 memeLi.appendChild(img);
 memeLi.appendChild(topTextDiv);
 memeLi.appendChild(bottomTextDiv);
-memeLi.appendChild(removeDiv);
+
 //memeLi.appendChild(removeButton);
 
 
